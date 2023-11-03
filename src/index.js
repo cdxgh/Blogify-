f452c043496844f0b111eaba5679c70b13daf2fe
CodeSandbox <support@codesandbox.io>
2023-11-03 06:18:21 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-var http = require("http");
+const path = require("path");
+const express = require("express");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get('/', (req, res)=>{
+  res.render("home");
+});
+
+app.listen(PORT, ()=>console.log(`server Started at PORT:${PORT}`));
+
+
