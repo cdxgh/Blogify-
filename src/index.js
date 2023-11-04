c87d212bebe8d1cf3d028340f8a88685d3b51d95
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-11-04 08:24:27 +0000

    Update 11 files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const path = require("path");
 const express = require("express");
 
+const userRoute = require("../routes/user");
+
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.get('/', (req, res)=>{
+app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(PORT, ()=>console.log(`server Started at PORT:${PORT}`));
-
-
+app.use("/user", userRoute);
+app.listen(PORT, () => console.log(`server Started at PORT:${PORT}`));
