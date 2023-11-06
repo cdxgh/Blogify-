3f3e722df1be8cf0d22421bc6a744125c9c77128
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-11-06 08:11:47 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const express = require("express");
-
+const mongoose = require("mongoose");
 const userRoute = require("../routes/user");
 
 const app = express();
 const PORT = 8080;
 
+// mongoose.connect('mongodb://127.0.0.1:27017/blogify')
+// .then((e) => console.log("MongoDb Connected"));
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
